e7d45849f6ca7b64cde2be36dd7f6168904dbd96
Demian <sodaoo@qq.com>
2022-07-17 00:32:04 +0800

    feat-axios update

---
+++ backend/README.md
@@ -0,0 +1,11 @@
+```
+Post:
+http://localhost:8080/api/articles/
+
+
+
+
+Get :
+http://localhost:8080/api/articles/
+```
+
--- frontend/src/pages/Articles/index.js
+++ frontend/src/pages/Articles/index.js
@@ -3,16 +3,29 @@
 // import ExampleComponent from '../../components/ExampleComponent'
 
 import Acticle from '../../components/Article';
-
+import { http } from '../../utils'
+import { useEffect } from 'react'
+/* Markdown 渲染条件: 
+1. 必须空首行 
+2. 左侧缩进不能太多 */
 const defaultMd = `
 
-* [x] azeazea
-  | Feature    | Support              |
-  | ---------- | -------------------- |
-  | CommonMark | 100%                 |
-  | GFM        | 100%                 |`;
+  * [x] azeazea
+
+    | Feature    | Support              |
+    | ---------- | -------------------- |
+    | CommonMark | 100%                 |
+    | GFM        | 100%                 |`;
 
 export default function Article() {  
+  // 获取文章列表
+  useEffect(() => {
+    const loadList = async () => {
+      const res = await http.get('/api/tutorials')
+      console.log("res",res)
+    }
+    loadList()
+  }, [])
   return (
     <Acticle content={defaultMd} />
   )
